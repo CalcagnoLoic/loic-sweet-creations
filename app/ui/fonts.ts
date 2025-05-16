8e3a92933e95a3bff318ee994e8beaabeea19b3a
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2025-05-16 14:33:32 +0200

    import styles + fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat } from "next/font/google";
+import { Sofia } from "next/font/google";
+
+export const montserrat = Montserrat({ subsets: ["latin"], weight: "400" });
+export const sofia = Sofia({ subsets: ["latin"], weight: "400" });
